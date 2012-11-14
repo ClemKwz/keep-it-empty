1f001451f0e0e728220767a81fcbf4deaedd091a
clement.kawczak@gmail.com <clement.kawczak@gmail.com@ffbe158c-51fe-5590-aa6e-4dd8532568ef>
2012-11-14 23:16:13 +0000

    Test gameplay with smaller explosions instead of the speed

--- trunk/KeepItEmpty/Level.cpp
+++ trunk/KeepItEmpty/Level.cpp
@@ -80,6 +80,16 @@ void Level::Update()
 							{
 								m_ppElements[i]->SetAlreadyMultiply(true);
 								m_ppElements[cpt] = new Element(m_pGame, m_ppElements[i]->GetPosX(), m_ppElements[i]->GetPosY(),m_pGame->nRadius, m_pGame->fSpeed);
+								for(int cpt2 = cpt+1;cpt2 < cpt+12;cpt2++)
+								{
+									if(cpt2 < m_nElements)
+									{
+										if(m_ppElements[cpt2]->GetState() == Dead)
+										{
+											m_ppElements[cpt2] = new Element(m_pGame, m_ppElements[i]->GetPosX(), m_ppElements[i]->GetPosY(),m_pGame->nRadius, m_pGame->fSpeed);
+										}
+									}
+								}
 								m_ppElements[cpt]->SetAlreadyMultiply(true);
 								cpt = m_nElements;
 							}
